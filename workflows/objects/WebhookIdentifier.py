43a97cc3b398389c01daf13501587232158b1c3e
dc <danni.co@outlook.fr>
2018-10-23 18:13:24 +0200

    detecting merge QRadar case

--- workflows/objects/WebhookIdentifier.py
+++ workflows/objects/WebhookIdentifier.py
@@ -131,6 +131,22 @@ class Webhook:
             return True
         else:
             return False
+
+    def isFromMergedCases(self):
+        """
+            Check if the webhook describes a case that comes from a merging action
+
+            :return: True if it is case the comes from a merging action
+            :rtype: boolean
+        """
+
+        self.logger.info('%s.isFromMergedCases starts', __name__)
+
+        if 'mergeFrom' in self.data['object']:
+            return True
+        else:
+            return False
+    
         
     def isQRadarAlertMarkedAsRead(self):
         """
@@ -160,6 +176,8 @@ class Webhook:
             If the case is merged, it is not considered to be closed (even if it is
             from TheHive perspective), as a result, a merged qradar case will not close
             an offense.
+            However a case created from merged case, where one of the merged case is
+            related to QRadar, will close the linked QRadar offense.
     
             :return: True if it is a QRadar alert marked as read, False if not
             :rtype: boolean
@@ -171,24 +189,22 @@ class Webhook:
             if self.isCase() and self.isClosed() and not self.isMergedInto():
                 #searching in alerts if the case comes from a QRadar alert
                 esCaseId = self.data['objectId']
-                query = dict()
-                query['case'] = esCaseId
-                results = self.theHiveConnector.findAlert(query)
-    
-                if len(results) == 1:
-                #should only have one hit
-                    if results[0]['source'] == 'QRadar_Offenses':
-                        #case opened from alert
-                        #and from QRadar
-                        self.offenseId = results[0]['sourceRef']
-                        return True
-                    else:
-                        #case opened from an alert but
-                        #not from QRadar
-                        return False
+                if self.fromQRadar(esCaseId):
+                    return True
                 else:
-                    #case not opened from an alert
-                    return False
+                    #at this point, the case was not opened from a QRadar alert
+                    #however, it could be a case created from merged cases
+                    #if one of the merged case is related to QRadar alert
+                    #then we consider the case as being from QRadar
+                    if self.isFromMergedCases():
+                        for esCaseId in self.data['object']['mergeFrom']:
+                            if self.fromQRadar(esCaseId):
+                                return True
+                        #went through all merged case and none where from QRadar
+                        return False
+                    else:
+                    #not a QRadar case
+                        return False
             else:
                 #not a case or have not been closed when
                 #when the webhook has been issued
@@ -198,3 +214,33 @@ class Webhook:
         except Exception as e:
             self.logger.error('%s.isClosedQRadarCase failed', __name__, exc_info=True)
             raise
+
+    def fromQRadar(self, esCaseId):
+        """
+            For a given esCaseId, search if the case has been opened from
+            a QRadar offense, if so adds the offenseId attribute to this object
+
+            :param esCaseId: elasticsearch case id 
+            :type esCaseId: str
+
+            :return: True if it is a QRadar case, false if not
+            :rtype: bool
+        """
+
+        query = dict()
+        query['case'] = esCaseId
+        results = self.theHiveConnector.findAlert(query)
+    
+        if len(results) == 1:
+        #should only have one hit
+            if results[0]['source'] == 'QRadar_Offenses':
+                #case opened from alert
+                #and from QRadar
+                self.offenseId = results[0]['sourceRef']
+                return True
+            else:
+                #case opened from an alert but
+                #not from QRadar
+                return False
+        else:
+            return False
