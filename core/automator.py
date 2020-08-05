425bd4a22b9ce521f7a4d7a10104d79f73ee72ec
Jeffrey Everling <jeffrey@antsec.io>
2020-08-05 15:20:58 +0200

    fixing module import

--- core/automator.py
+++ core/automator.py
@@ -24,7 +24,7 @@ modules_dir = app_dir + "/../modules"
 loaded_modules = {}
 
 #Read all modules from modules folder
-modules = [ name for name in os.listdir(modules_dir) if os.path.isdir(os.path.join(modules_dir, name)) if os.path.isfile(os.path.join(modules_dir, "automators.py")) ]
+modules = [ name for name in os.listdir(modules_dir) if os.path.isdir(os.path.join(modules_dir, name)) if os.path.isfile(os.path.join(modules_dir, name, "automators.py")) ]
 
 logger.info("Loading modules for automation")
 #Loop through modules to create a module dictionary
