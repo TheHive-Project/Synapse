854ee611052bb2182b66de536dbe1f15bd0c243b
Jeffrey Everling <jeffrey@antsec.io>
2020-08-05 17:28:03 +0200

    Fixing endpoint generation

--- app.py
+++ app.py
@@ -78,13 +78,13 @@ def listenWebhook():
 #loop through all integration modules and create the corresponding endpoints
 for cfg_section in cfg.sections():
     endpoint = cfg.get(cfg_section, 'synapse_endpoint', fallback=None)
-    logger.info("Creating endpoint for: {}".format(endpoint))
     if endpoint:
+        logger.info("Creating endpoint for: {}. Endpoint: {}".format(cfg_section, endpoint))
         methods = cfg.get(cfg_section, 'endpoint_methods', fallback='GET').split(',')
         @app.route(endpoint, methods=methods)
         def endpoint():
-            self.module = cfg_section
-            response = loaded_modules[self.module].validateRequest(request)
+            module = cfg_section
+            response = loaded_modules[module].validateRequest(request)
             return response
 
 @app.route('/version', methods=['GET'])
