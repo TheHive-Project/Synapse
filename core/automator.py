d269536080996dbfc533a13270342f954bebea47
Jeffrey Everling <jeffrey@antsec.io>
2020-08-11 16:19:58 +0200

    Moving automation to modules

--- core/automator.py
+++ core/automator.py
@@ -2,13 +2,10 @@ import logging
 import os
 from datetime import datetime, timezone, timedelta
 import pytz
-import requests
-import json
 from configparser import ConfigParser
 from thehive4py.models import CaseTask, Alert
 
 from modules.TheHive.connector import TheHiveConnector
-from modules.Cortex.connector import CortexConnector
 
 from core.loader import moduleLoader
 loaded_modules = moduleLoader("automator")
@@ -35,8 +32,6 @@ class Automator:
         self.app_dir = os.path.dirname(os.path.abspath(__file__)) + "/.."
         self.use_case_config = use_cases
         self.TheHiveConnector = TheHiveConnector(cfg)
-        if self.cfg.getboolean('Cortex', 'enabled'):
-            self.CortexConnector = CortexConnector(cfg)
         self.webhook = webhook
         self.root_id = self.webhook.data['rootId']
 
@@ -88,7 +83,6 @@ class Automator:
                 for action, action_config in self.use_case_actions.items():
                     self.action_config = action_config
                     #Give automator information regarding the webhook as some actions are limited to the state of the alert/case
-                    self.action_config['webhook'] == "importedAlert":
                     self.logger.info('Found the following action for %s: %s, with type %s' % (self.rule_id, action, action_type))
                     self.case_id = self.webhook.data['object']['case']
                     
@@ -100,104 +94,6 @@ class Automator:
 
             else:
                 self.logger.info('Did not find any matching use cases for %s' % tag)
-
-                    #Define customer_id
-                    if self.cfg.getboolean('UCAutomation','enable_customer_list', fallback=False):
-                        if 'internal' in self.use_cases[self.rule_id] and self.use_cases[self.rule_id]['internal']:
-                            self.logger.info("Rule %s is marked as internal" % self.rule_id)
-                            self.customer_id = self.use_case_config['configuration']['internal_contact']
-                        elif 'debug' in self.use_cases[self.rule_id] and self.use_cases[self.rule_id]['debug']:
-                            self.logger.info("Using debug settings for rule %s" % self.rule_id)
-                            self.customer_id = self.use_case_config['configuration']['debug_contact']
-                        else:
-                            self.customer_id = self.MatchValueAgainstTags(self.tags, self.customers)
-
-                    ## Try to retrieve the defined actions
-                    self.use_case_actions = self.use_cases[self.rule_id]['automation']
-                    #perform actions defined for the use case
-                    for action, action_config in self.use_case_actions.items():
-                        action_type = action_config['type']
-                        self.logger.info('Found the following action for %s: %s, with type %s' % (self.rule_id, action, action_type))
-                        self.case_id = self.webhook.data['object']['id']
-
-                        #Perform actions for the checkSiem action
-                        if action_type == "checkSiem":
-                            if self.checkSiem(action_config, "enrichment_query"):
-                                self.report_action = 'updateAlert'
-                        elif action_type == "SendNotificationFromAlert":
-                            self.case_description = self.webhook.data['object']['description']
-                            self.title = action_config['title']
-
-                            if "email" in action_config['platforms']:
-                                self.notification_type = "email"
-                                self.description = self.craftDescription(self.mailsettings, action_config['long_template'], self.tags, self.case_description, self.customer_id, self.notification_type)
-                                self.logger.info('Found alert to send mail for: %s' % self.title)
-
-                                self.data = {
-                                    "data": {
-                                        "description": self.description,
-                                        "title": self.title
-                                    },
-                                    "dataType":"thehive:case_task"
-                                }
-
-                                self.logger.info('Sending mail for alert with id: %s' % self.alert_id)
-                                self.CortexConnector.runResponder(self.use_case_config['configuration']['mail']['responder_id'], self.data)
-                            #Posting to slack
-                            if "slack" in action_config['platforms']:
-                                #Fallback for notification where no short_template is available
-                                if 'short_template' in action_config:
-                                    template = action_config['short_template']
-                                else:
-                                    template = action_config['long_template']
-
-                                self.notification_type = "slack"
-                                self.description = self.craftDescription(self.mailsettings, template, self.tags, self.case_description, self.customer_id, self.notification_type)
-                                try:
-                                    self.slack_url = self.customer_cfg.get(self.customer_id, 'slack_url')
-                                except:
-                                    self.logger.error("Could not retrieve slack url for customer %s" % self.customer_id)
-                                
-                                if hasattr(self, 'slack_url'):
-                                    self.slack_data = {}
-                                    self.slack_data['text'] = "*%s*\n %s" % (self.title, self.description)
-
-                                    try:
-                                        self.response = requests.post(self.slack_url, data=json.dumps(self.slack_data), headers={'Content-Type': 'application/json'})
-                                        if self.response.status_code not in [200, 201]:
-                                            self.logger.error("Something went wrong when posting to slack: %s" % self.response.raw.read())
-                                    except Exception as e:
-                                        self.logger.error("Could not post alert to Slack", exc_info=True)
-                            #Posting to teams
-                            if "teams" in action_config['platforms']:
-                                #Fallback for notification where no short_template is available
-                                if 'short_template' in action_config:
-                                    template = action_config['short_template']
-                                else:
-                                    template = action_config['long_template']
-
-                                self.notification_type = "teams"
-                                self.description = self.craftDescription(self.mailsettings, template, self.tags, self.case_description, self.customer_id, self.notification_type)
-                                try:
-                                    self.teams_url = self.customer_cfg.get(self.customer_id, 'teams_url')
-                                except:
-                                    self.logger.error("Could not retrieve teams url for customer %s" % self.customer_id)
-                                
-                                if hasattr(self, 'teams_url'):
-                                    self.teams_data = {}
-                                    self.teams_data['text'] = "***%s***</br><pre>%s</pre>" % (self.title, self.description)
-
-                                    try:
-                                        self.response = requests.post(self.teams_url, data=json.dumps(self.teams_data), headers={'Content-Type': 'application/json'})
-                                        if self.response.status_code not in [200, 201]:
-                                            self.logger.error("Something went wrong when posting to teams: %s" % self.response.raw.read())
-                                    except Exception as e:
-                                        self.logger.error("Could not post alert to teams", exc_info=True)
-                        else:
-                            self.logger.info('Did not find any supported actions')
-
-                else:
-                    self.logger.info('Did not find any matching use cases for %s' % tag)
 
         return self.report_action
 
--- modules/TheHive/automator.py
+++ modules/TheHive/automator.py
@@ -1,6 +1,9 @@
-
 import logging
+import requests
+import json
+
 from modules.TheHive.connector import TheHiveConnector
+from modules.Cortex.connector import CortexConnector
 from thehive4py.models import CaseTask
 
 class Automators():
@@ -10,6 +13,8 @@ class Automators():
 
         self.cfg = cfg
         self.TheHiveConnector = TheHiveConnector(cfg)
+        if self.cfg.getboolean('Cortex', 'enabled'):
+            self.CortexConnector = CortexConnector(cfg)
 
     '''
     Can be used to check if there is a match between tags and the provided list.
@@ -36,7 +41,7 @@ class Automators():
         
         try:
             #The tag should match this regex to find the value
-            value = re.search(self.value_regex, webhook.data['object']['description']).group(1)
+            value = re.search(self.value_regex, webhook_data['object']['description']).group(1)
             return value
         except:
             return False
@@ -109,6 +114,18 @@ class Automators():
         
         return self.description
 
+    def checkCustomerId(self):
+        if 'internal' in self.use_cases[self.rule_id] and self.use_cases[self.rule_id]['internal']:
+            self.logger.info("Rule %s is marked as internal" % self.rule_id)
+            self.results = self.use_case_config['configuration']['internal_contact']
+        elif 'debug' in self.use_cases[self.rule_id] and self.use_cases[self.rule_id]['debug']:
+            self.logger.info("Using debug settings for rule %s" % self.rule_id)
+            self.results = self.use_case_config['configuration']['debug_contact']
+        else:
+            self.results = self.MatchValueAgainstTags(self.tags, self.customers)
+            
+        return self.results
+
     def createBasicTask(self, action_config, webhook_data):
         #Only continue if the right webhook is triggered
         if not self.webhook_data.isImportedAlert():
@@ -136,7 +153,7 @@ class Automators():
         self.customer_id = self.MatchValueAgainstTags(self.tags, self.customers)
         self.logger.info('Found customer %s, retrieving recipient' % self.customer_id)
         self.notification_type = "email"
-        self.case_description = self.webhook.data['object']['description']
+        self.case_description = self.webhook_data['object']['description']
         self.title = action_config['title']
         self.description = self.craftDescription(self.mailsettings, action_config['long_template'], self.tags, self.case_description, self.customer_id, self.notification_type)
 
@@ -148,3 +165,76 @@ class Automators():
         if action_config['auto_send_mail'] and not self.stopsend:
             self.logger.info('Sending mail for task with id: %s' % self.ucTaskId)
             self.TheHiveConnector.runResponder('case_task', self.ucTaskId, self.use_case_config['configuration']['mail']['responder_id'])
+
+    def SendNotificationFromAlert(self, action_config, webhook_data)::
+        self.case_description = self.webhook_data['object']['description']
+        self.title = action_config['title']
+
+        if self.cfg.getboolean('UCAutomation','enable_customer_list', fallback=False):
+            self.customer_id = self.checkCustomerId()
+
+        if "email" in action_config['platforms']:
+            self.notification_type = "email"
+            self.description = self.craftDescription(self.mailsettings, action_config['long_template'], self.tags, self.case_description, self.customer_id, self.notification_type)
+            self.logger.info('Found alert to send mail for: %s' % self.title)
+
+            self.data = {
+                "data": {
+                    "description": self.description,
+                    "title": self.title
+                },
+                "dataType":"thehive:case_task"
+            }
+
+            self.logger.info('Sending mail for alert with id: %s' % self.alert_id)
+            self.CortexConnector.runResponder(self.use_case_config['configuration']['mail']['responder_id'], self.data)
+        #Posting to slack
+        if "slack" in action_config['platforms']:
+            #Fallback for notification where no short_template is available
+            if 'short_template' in action_config:
+                template = action_config['short_template']
+            else:
+                template = action_config['long_template']
+
+            self.notification_type = "slack"
+            self.description = self.craftDescription(self.mailsettings, template, self.tags, self.case_description, self.customer_id, self.notification_type)
+            try:
+                self.slack_url = self.customer_cfg.get(self.customer_id, 'slack_url')
+            except:
+                self.logger.error("Could not retrieve slack url for customer %s" % self.customer_id)
+            
+            if hasattr(self, 'slack_url'):
+                self.slack_data = {}
+                self.slack_data['text'] = "*%s*\n %s" % (self.title, self.description)
+
+                try:
+                    self.response = requests.post(self.slack_url, data=json.dumps(self.slack_data), headers={'Content-Type': 'application/json'})
+                    if self.response.status_code not in [200, 201]:
+                        self.logger.error("Something went wrong when posting to slack: %s" % self.response.raw.read())
+                except Exception as e:
+                    self.logger.error("Could not post alert to Slack", exc_info=True)
+        #Posting to teams
+        if "teams" in action_config['platforms']:
+            #Fallback for notification where no short_template is available
+            if 'short_template' in action_config:
+                template = action_config['short_template']
+            else:
+                template = action_config['long_template']
+
+            self.notification_type = "teams"
+            self.description = self.craftDescription(self.mailsettings, template, self.tags, self.case_description, self.customer_id, self.notification_type)
+            try:
+                self.teams_url = self.customer_cfg.get(self.customer_id, 'teams_url')
+            except:
+                self.logger.error("Could not retrieve teams url for customer %s" % self.customer_id)
+            
+            if hasattr(self, 'teams_url'):
+                self.teams_data = {}
+                self.teams_data['text'] = "***%s***</br><pre>%s</pre>" % (self.title, self.description)
+
+                try:
+                    self.response = requests.post(self.teams_url, data=json.dumps(self.teams_data), headers={'Content-Type': 'application/json'})
+                    if self.response.status_code not in [200, 201]:
+                        self.logger.error("Something went wrong when posting to teams: %s" % self.response.raw.read())
+                except Exception as e:
+                    self.logger.error("Could not post alert to teams", exc_info=True)
